995faad7309b4dbef36be01ddbc66e9286d75e5b
Dilipqr <69391421+Dilipqr@users.noreply.github.com>
2020-08-11 20:06:45 +0530

    straining 03-08

--- s3-08.py
+++ s3-08.py
@@ -37,11 +37,11 @@ for i in range(1,10):
         print(i)'''
 
 #pgm to display the numbers from 1 to 10  except from 3,5,7.
-for i in range(1,11):
+'''for i in range(1,11):
     if(i==3):
         continue
     elif(i==5):
         continue
     elif(i==7):
         continue
-    print(i,end=" ")
+    print(i,end=" ")'''
